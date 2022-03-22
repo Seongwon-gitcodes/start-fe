95eb91bc673d751d6d1c100c11d30c697c19fd64
seongwon-gitcodes <ysw991106@gmail.com>
2022-03-22 15:14:51 +0900

    update 03

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-var me = {name : "양성원", age : 24};     var ability = {language: ["java", "python", "js"], excercise: ["농구", "축구"]};
+// var count = [10,20,30,40,50,60,70,100];
+// for(var x in count) {
+//     var result = 0;
+//     result += x;    
+// }
+// avg = result/count.length;
+// console.log(avg)
+
+
+
+var todos = ['운동', '게임'];
+var updateTodo = '게임';
+var updateIndex = todos.findIndex(function(todo) {
+  return todo === updateTodo;
+});
+todos[updateIndex] = '공부';
+console.log(todos);
